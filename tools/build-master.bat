9e62baa12e71da5f03481d8e66dce69ef6104453
weiming.ma <weimingma6@outlook.com>
2024-10-06 23:10:00 +0800

    update build flow

--- tools/build-master.bat
+++ tools/build-master.bat
@@ -1,4 +1,17 @@
+@echo off
+setlocal enabledelayedexpansion
+
 call env_setting.bat
+
+:GetComPort
+    set "COM_PORT="
+    set "COM_COUNT=0"
+
+    :: get com port and count
+    for /f "tokens=*" %%A in ('wmic path Win32_SerialPort get DeviceID ^| findstr /R "^COM[0-9]"') do (
+        set /a COM_COUNT+=1
+        set COM_PORT=%%A
+    )
 
 cd ../source/master
 
@@ -12,5 +25,40 @@ if "%~1"=="clean" (
     exit /b
 )
 
-echo "start building"
-idf.py build
+if "%~1"=="build" (
+    echo "start building"
+    idf.py set-target esp32s3
+    idf.py build
+    exit /b
+)
+
+if "%~1"=="flash" (
+    if !COM_COUNT! equ 1 (
+        echo only have one device: !COM_PORT!
+    ) else if !COM_COUNT! gtr 1 (
+        echo ERROR: TOO MANY DEVICES
+        exit /b
+    ) else (
+        echo ERROR: NO DEVICE CONNECTED
+        exit /b
+    )
+
+    idf.py -p %COM_PORT% flash
+    exit /b
+)
+
+if "%~1"=="run" (
+    if !COM_COUNT! equ 1 (
+        echo only have one device: !COM_PORT!
+    ) else if !COM_COUNT! gtr 1 (
+        echo ERROR: TOO MANY DEVICES
+        exit /b
+    ) else (
+        echo ERROR: NO DEVICE CONNECTED
+        exit /b
+    )
+
+    idf.py -p %COM_PORT% monitor
+    exit /b
+)
+
--- tools/env_setting.bat
+++ tools/env_setting.bat
@@ -1,3 +1,1 @@
 D:\Espressif\idf_cmd_init.bat esp-idf-f67c265457a2080395f70a27180a536c
-
-idf.py set-target esp32s3
